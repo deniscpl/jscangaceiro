51307d09630c8a1fad71d4dcc396deb4e259a951
Denis <denis_cpl@hotmail.com>
2020-11-04 20:58:36 -0300

    Classe Negociação

---
+++ client/app/domain/negociacao/Negociacao.js
@@ -0,0 +1,28 @@
+class Negociacao {
+    constructor(_data, _quantidade, _valor) {
+    Object.assign(this, { _quantidade, _valor });
+    this._data = new Date(_data.getTime());
+    Object.freeze(this);
+    }
+
+
+    get Volume() {
+
+        return this._quantidade * this._valor;
+    }
+
+    get Data() {
+
+        return new Date(this._data.getTime());
+    }
+
+    get Quantidade() {
+
+        return this._quantidade
+    }
+
+    get Valor() {
+        return this._valor;
+    }
+
+}
