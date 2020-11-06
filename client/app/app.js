9ea9f969ec66d5bb61e20c1decccce59077393de
Denis <denis_cpl@hotmail.com>
2020-11-05 21:47:50 -0300

    Classe Controller

---
+++ client/app/app.js
@@ -0,0 +1,3 @@
+let controller = new NegociacaoController();
+// associa o evento de submissão do formulário à chamada do método "adiciona"
+document.querySelector('.form').addEventListener('submit', controller.adiciona);
