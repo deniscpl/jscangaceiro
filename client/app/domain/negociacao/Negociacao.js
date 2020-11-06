ed67867f1c139489b7fa69d01ab979cb9323e7b8
Denis <denis_cpl@hotmail.com>
2020-11-05 21:46:38 -0300

    Versao final

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -1,10 +1,10 @@
 class Negociacao {
+
     constructor(_data, _quantidade, _valor) {
     Object.assign(this, { _quantidade, _valor });
     this._data = new Date(_data.getTime());
     Object.freeze(this);
     }
-
 
     get Volume() {
 
