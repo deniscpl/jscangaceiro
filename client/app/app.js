8a1476ea9b912dc3720c4e687722105fab2fe695
Denis <denis_cpl@hotmail.com>
2020-11-17 22:00:24 -0300

    Criação da classe

--- client/app/app.js
+++ client/app/app.js
@@ -1,3 +1,3 @@
 let controller = new NegociacaoController();
-// associa o evento de submissão do formulário à chamada do método "adiciona"
-document.querySelector('.form').addEventListener('submit', controller.adiciona);
+
+document.querySelector('.form').addEventListener('submit', controller.adiciona.bind(controller));
--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,17 +1,38 @@
 class NegociacaoController {
-    adiciona(event) {        
 
-        event.preventDefault();       
-        let $ =  document.querySelector.bind(document);    
+    constructor () {
 
-        let inputData = $('#data');
-        let inputQuantidade = $('#quantidade');
-        let inputValor = $('#valor');
+        let $ =  document.querySelector.bind(document);   
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+        this._negociacoes = new Negociacoes();
 
-        console.log(inputData.value);
-        console.log(parseInt(inputQuantidade.value));
-        console.log(parseFloat(inputValor.value));
+    }
 
+    adiciona(event) {
+
+        event.preventDefault();
+        this._negociacoes.adiciona(this._criaNegociacao());        
+        this._limpaFormulario();
+    }
+
+    _limpaFormulario() {
+
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = '0.0';
+        this._inputData.focus();
+
+    }
+
+    _criaNegociacao() {
+
+        return new Negociacao(
+            DateConverter.paraData(this._inputData.value),
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        );
     }
 
 }
